1817bb49e6930175fc5a693ac83c82d9745ad0c2
Punny <punza15@hotmail.com>
2022-10-29 21:18:39 +0700

    Updated 29/10/2022
    Updated all files related to NoSQL and Ontology

--- NoSQL Assignment/Netflix_query.js
+++ NoSQL Assignment/Netflix_query.js
@@ -1,7 +1,12 @@
 use netflix_db
 
-db.Netflix.find({})
+/* Query 1 */
+db.Netflix.find({rating: R},{title:1,type:1,rating:1}).sort({title:1})
 
+/* Query 2 */
+db.Netflix.find({release_year: {$gte: 2010}, genres: {$regex: /^Action.*/}},{type:1, title:1, director:1, cast:1, country:1, rating:1, duration:1, genres:1, description:1})
+
+/* Query 3 */
 db.Netflix.find({
     $and: [
         {$or: [
